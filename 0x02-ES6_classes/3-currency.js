57bab389b3ad7bf17ad7bd544e691d3e4e13e68c
azeadrr <azeadn1@gmail.com>
2024-01-04 05:48:07 -0800

    mandatory tasks

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -7,15 +7,19 @@ export default class Currency {
   get code() {
     return this._code;
   }
+
   set code(code) {
     this._code = code;
   }
+
   get name() {
     return this._name;
   }
+
   set name(newName) {
     this._name = newName;
   }
+
   displayFullCurrency() {
     return `${this._name} (${this._code})`;
   }
