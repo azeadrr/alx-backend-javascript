f03eec2d4ba2930a44253cd04aca1355189b998d
azeadrr <azeadn1@gmail.com>
2024-01-04 05:28:13 -0800

    mandatory tasks

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,46 @@
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    if (typeof name === 'string') {
+      this._name = name;
+    } else {
+      throw new TypeError('Name must be a string');
+    }
+    if (typeof length === 'number') {
+      this._length = length;
+    } else {
+      throw new TypeError('Length must be a number');
+    }
+    if (Array.isArray(students)) {
+      this._students = students;
+    } else {
+      throw new TypeError('Students must be an array');
+    }
+  }
+  get name() {
+    return this._name;
+  }
+  set name(newName) {
+    if (typeof newName !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    this._name = newName;
+  }
+  get length() {
+    return this._length;
+  }
+  set length(newLength) {
+    if (typeof newLength !== 'number') {
+      throw new TypeError('Length must be a number');
+    }
+    this._length = newLength;
+  }
+  get students() {
+    return this._students;
+  }
+  set students(newStudents) {
+    if (!Array.isArray(newStudents)) {
+      throw new TypeError('Students must be an array');
+    }
+    this._students = newStudents;
+  }
+}
