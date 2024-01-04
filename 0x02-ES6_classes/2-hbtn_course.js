0fe45630f1c6249303efbad096ab683ff2b18940
azeadrr <azeadn1@gmail.com>
2024-01-04 15:07:12 -0800

    updated

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,7 +1,7 @@
 export default class HolbertonCourse {
   constructor(name, length, students) {
-    this._name = this._validStr(name);
-    this._length = this._validNum(length);
+    this._name = this._validString(name);
+    this._length = this._validNumber(length);
     this._students = this._validArray(students);
   }
 
@@ -10,7 +10,7 @@ export default class HolbertonCourse {
   }
 
   set name(name) {
-    this._name = this._validStr(name);
+    this._name = this._validString(name);
   }
 
   get length() {
@@ -18,7 +18,7 @@ export default class HolbertonCourse {
   }
 
   set length(length) {
-    this._length = this._validNum(length);
+    this._length = this._validNumber(length);
   }
 
   get students() {
@@ -29,8 +29,7 @@ export default class HolbertonCourse {
     this._students = this._validArray(students);
   }
 
-  // Disable next line methods in the class methods
-  _validStr(value) {
+  _validString(value) {
     if (typeof value !== 'string') {
       throw new TypeError('Name must be a string');
     } else {
@@ -38,8 +37,7 @@ export default class HolbertonCourse {
     }
   }
 
-  // Disable next line methods in the class methods
-  _validNum(value) {
+  _validNumber(value) {
     if (typeof value !== 'number') {
       throw new TypeError('Length must be a number');
     } else {
@@ -47,7 +45,6 @@ export default class HolbertonCourse {
     }
   }
 
-  // Disable next line methods in the class methods
   _validArray(arr) {
     if (!Array.isArray(arr) || arr.some((item) => typeof item !== 'string')) {
       throw new TypeError('Students must be an array of strings');
