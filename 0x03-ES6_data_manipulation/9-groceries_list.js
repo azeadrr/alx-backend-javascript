a3df0a62c4c5544431f9adae9437ef9f6ebbb46b
azeadrr <azeadn1@gmail.com>
2024-01-08 14:17:47 -0800

    mandatory tasks

---
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,9 @@
+export default function groceriesList() {
+  const grcMap = new Map();
+  grcMap.set('Apples', 10);
+  grcMap.set('Tomatoes', 10);
+  grcMap.set('Pasta', 1);
+  grcMap.set('Rice', 1);
+  grcMap.set('Banana', 5);
+  return grcMap;
+}
